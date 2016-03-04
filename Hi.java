66f8530aa667001809753728bb75044cb8ffb628
DaniilZavadskiy <danilzavadskiy63@gmail.com>
2016-03-05 00:31:35 +0200

    Added files via upload

---
+++ Hi.java
@@ -0,0 +1,7 @@
+
+public class Hi {
+	public static void main(String[] args) {
+        // Display "Hi, I'm your first Java program! Stay tuned!"
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+	}
+}
